89c445d70b592b0e52b14c8d752d17afa4e23452
Gonzalo <gonzalo.cisternas@ug.uchile.cl>
2023-10-15 10:28:30 -0300

    Algoritmo de construcción Nearest-X

--- algoritmo1.cpp
+++ algoritmo1.cpp
@@ -23,6 +23,9 @@ struct Rectangle {
     int distY(){
         return abs(y2 - y1);
     }
+    float centerX() {
+        return (x1 + x2) / 2.0f;
+    }
 };
 
 //Se crea función que recibe un vector de rectángulos y los ordena según la coordenada X del centro del rectángulo
@@ -53,12 +56,28 @@ Node* makeParent(vector<Node*> children) {
     Node* node = new Node;
     node->isLeaf = false;
     node->children = children;
+
     //Se calcula el rectángulo que contiene a todos los hijos
-    int x1 = children[0]->MBR.x1;
-    int y1 = children[0]->MBR.y1;
-    int x2 = children[children.size() -1]->MBR.x2;
-    int y2 = children[children.size() -1]->MBR.y2;
-    Rectangle rect = {x1, y1, x2, y2};
+    int x_chiquito = children[0]->MBR.x1; // pivote
+    int x_grande = children[0]->MBR.x2;
+    int y_chiquito = children[0]->MBR.y1;
+    int y_grande = children[0]->MBR.y2;
+    for (int i = 0; i < children.size(); i++) {
+
+        if (children[i]->MBR.x1 < x_chiquito)
+            x_chiquito = children[i]->MBR.x1;
+
+        if (children[i]->MBR.x2 > x_grande)
+            x_grande = children[i]->MBR.x2;
+
+        if (children[i]->MBR.y1 < y_chiquito)
+            y_chiquito = children[i]->MBR.y1;
+
+        if (children[i]->MBR.y2 > y_grande)
+            y_grande = children[i]->MBR.y2;
+    }
+
+    Rectangle rect = {x_chiquito, y_chiquito, x_grande, y_grande};
     node->MBR = rect;
     return node;
 }
@@ -68,35 +87,82 @@ struct RTree {
     int M;
     vector<Node*> hijos;
 };
-//Función que recibe un vector de rectángulos y un entero M, y crea un R-tree
-RTree* makeRTree(vector<Rectangle> rects, int M) {
-    RTree* tree = new RTree;
-    tree->M = M;
-    //Se ordenan los rectángulos según la coordenada X del centro del rectángulo
+//Función que crea hojas a partir de un vector de rectángulos que primero ordena
+vector<Node*> makeLeaves(vector<Rectangle> rects) {
+    vector<Node*> leaves;
     ordenarRectangulosX(rects);
-    //Se crea un vector de nodos hoja
-    vector<Node*> leafs;
     for (int i = 0; i < rects.size(); i++) {
-        //Se crea un nodo hoja por cada rectángulo
         Node* node = makeLeaf(rects[i]);
-        //Se agrega el nodo hoja al vector de nodos hoja
-        leafs.push_back(node);
+        leaves.push_back(node);
     }
-
-    int ciclos = ceil(log10(leafs.size())/log10(M));
-    for (int i = 0; i < ciclos; i++){
-        //Se crea un vector de nodos
-        for (int j = 0; j < leafs.size(); j++){
-            for (int k=0; k<M; k++){
-                vector<Node*> node;
-                Node* primero = leafs[0];
-                leafs = leafs.erase(leafs.begin());
-                node[k] = primero;
-
-
-            }
-        }
-        
-        
-    }
+    return leaves;
 }
+//Función que recibe un vector de nodos y hace grupos 
+RTree makeGroups(vector<Node*> children, int M){
+    vector<Node*> grupo; //
+    if (children.size()==1){
+        RTree rtree = RTree();
+        rtree.hijos=children;
+        rtree.root=children[0];
+        return rtree;
+    }
+    for (int i=0; i<(int)ceil((double)children.size()/M); i++){
+        vector<Node*> grupito;
+        for (int j=0; j<M; j++){
+            int index = i*M+j;
+            if (index >= children.size()){
+                break;
+            }
+            grupito.push_back(children[index]);
+        }
+        Node* node = makeParent(grupito);
+        
+        grupo.push_back(node);
+        cout << "Nodo grupito: " << node->MBR.x1 << ", " << node->MBR.x2 <<endl;
+        for (int k=0; k<grupito.size(); k++){
+            cout << "Nodo hoja: " << grupito[k]->MBR.x1 << ", " << grupito[k]->MBR.x2 <<endl;
+        }
+        grupito.clear();
+    }
+    return makeGroups(grupo, M);
+}
+
+//Main que genera aleatoriamente 6 rectángulos en un plano de 20x20 y los agrupa en un R-tree
+//utiliza include random
+int main(){
+    random_device rd;
+    mt19937 eng(rd());
+    uniform_int_distribution<> distr(0, 20);
+    //Se crea un vector de rectángulos
+    vector<Rectangle> rects;
+    //Se crean 6 rectángulos aleatorios
+    for (int i = 0; i < 7; i++) {
+        int a = distr(eng);
+        int b = distr(eng);
+        int c = distr(eng);
+        int d = distr(eng);
+        int x1 = min(a, c);
+        int x2 = max(a, c);
+        int y1 = min(b, d);
+        int y2 = max(b, d);
+        Rectangle rect = {x1, y1, x2, y2};
+        rects.push_back(rect);
+        cout << "Rectangulo " << i+1 << ": " << x1 << ", " << x2 << " " << y1 << ", " << y2 << endl;
+
+    }
+    // Se hacen las hojas
+    vector<Node*> leaves = makeLeaves(rects);
+    for (int i=0; i<7; i++) {
+        cout << "Centro: " << leaves[i]->MBR.centerX() << endl;
+    }
+    //Se crea el R-tree
+    RTree rtree = makeGroups(leaves, 3);
+    //Impresión de los nodos hoja
+    cout << "Nodos hoja: " << endl;
+
+    cout << "Nodo " << ": " << rtree.root->MBR.x1 << ", " << rtree.root->MBR.x2 <<endl;
+
+  
+
+}
+
