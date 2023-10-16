4e64b0f142fabd0f051403ff9c4c871aeb1c4543
Gonzalo <gonzalo.cisternas@ug.uchile.cl>
2023-10-16 11:46:40 -0300

    Disk read and write implemented

--- prompt.cpp
+++ prompt.cpp
@@ -24,8 +24,110 @@
 #include <vector>
 #include <iostream>
 #include <ostream>
+#include <algorithm>
+#include <random>
+#include <math.h>
 #include "algoritmo1.hpp"
 using namespace std;
+
+
+// Se crea función que recibe un vector de rectángulos y los ordena según la coordenada X del centro del rectángulo
+void ordenarRectangulosX(vector<Rectangle> &rects){
+    sort(rects.begin(), rects.end(), [](const Rectangle &a, const Rectangle &b){
+        float mean_a = (a.x1 + a.x2) / 2.0f;
+        float mean_b = (b.x1 + b.x2) / 2.0f;
+        return mean_a < mean_b; });
+}
+
+// Función que recibe un rectángulo y lo hace un nodo
+Node *makeLeaf(Rectangle rect){
+    Node *node = new Node;
+    node->MBR = rect;
+    node->isLeaf = true;
+    return node;
+}
+
+// Función que agrupa M nodos en un nodo padre
+Node *makeParent(vector<Node *> children){
+    Node *node = new Node;
+    node->isLeaf = false;
+    node->children = children;
+
+    // Se calcula el rectángulo que contiene a todos los hijos
+    int x_chiquito = children[0]->MBR.x1; // pivote
+    int x_grande = children[0]->MBR.x2;
+    int y_chiquito = children[0]->MBR.y1;
+    int y_grande = children[0]->MBR.y2;
+    for (int i = 0; i < children.size(); i++){
+
+        if (children[i]->MBR.x1 < x_chiquito)
+            x_chiquito = children[i]->MBR.x1;
+
+        if (children[i]->MBR.x2 > x_grande)
+            x_grande = children[i]->MBR.x2;
+
+        if (children[i]->MBR.y1 < y_chiquito)
+            y_chiquito = children[i]->MBR.y1;
+
+        if (children[i]->MBR.y2 > y_grande)
+            y_grande = children[i]->MBR.y2;
+    }
+
+    Rectangle rect = {x_chiquito, y_chiquito, x_grande, y_grande};
+    node->MBR = rect;
+    return node;
+}
+
+// Función que crea hojas a partir de un vector de rectángulos que primero ordena
+vector<Node *> makeLeaves(vector<Rectangle> rects){
+    vector<Node *> leaves;
+    ordenarRectangulosX(rects);
+    for (int i = 0; i < rects.size(); i++){
+        Node *node = makeLeaf(rects[i]);
+        leaves.push_back(node);
+    }
+    return leaves;
+}
+
+// Función que recibe un vector de nodos y hace grupos
+RTree makeGroups(vector<Node *> children, int M){
+    vector<Node *> grupo; //
+    if (children.size() == 1){
+        RTree rtree = RTree();
+        rtree.hijos = children;
+        rtree.root = children[0];
+        return rtree;
+    }
+    for (int i = 0; i < (int)ceil((double)children.size() / M); i++){
+        vector<Node *> grupito;
+        for (int j = 0; j < M; j++){
+            int index = i * M + j;
+            if (index >= children.size()){
+                break;
+            }
+            grupito.push_back(children[index]);
+        }
+        Node *node = makeParent(grupito);
+
+        grupo.push_back(node);
+        cout << "Nodo grupito: " << node->MBR.x1 << ", " << node->MBR.x2 << endl;
+        for (int k = 0; k < grupito.size(); k++){
+            //cout << "Nodo hoja: " << grupito[k]->MBR.x1 << ", " << grupito[k]->MBR.x2 << endl;
+        }
+        grupito.clear();
+    }
+    return makeGroups(grupo, M);
+}
+
+// Función Nearest-X que construye un R-tree a partir de un vector de rectángulos y un parámetro M
+RTree nearestX(vector<Rectangle> rects, int M){
+    // Se crean las hojas
+    vector<Node *> leaves = makeLeaves(rects);
+    // Se crea el R-tree
+    RTree rtree = makeGroups(leaves, M);
+    return rtree;
+}
+
 
 const string RTreeFile = "rtree.bin";
 int lastBlock = 0;
@@ -62,83 +164,62 @@ RTree readRTreeFromFile(int &diskAccesses) {
     return tree;
 }
 
-// 2. Ajustar búsqueda para contar accesos a disco
+vector<Rectangle> createRandomRectangles(int n){
+    vector<Rectangle> rects;
+    random_device rd;
+    mt19937 gen(rd());
 
-// Actualizaremos el método searchRecursive para contar los accesos a disco.
+    //Valores aleatorios entre 0 y 500mil
+    uniform_int_distribution<> distr1(0, 500000);
+    //El tamaño de los lados de los rectángulos es aleatorio entre 0 y 100
+    uniform_int_distribution<> distr2(0, 100);
 
-// cpp
+    // //Valores aleatorios entre 0 y 20
+    // uniform_int_distribution<> distr1(0, 20);
+    // //El tamaño de los lados de los rectángulos es aleatorio entre 0 y 10
+    // uniform_int_distribution<> distr2(0, 10);
 
-void searchRecursive(Node* currentNode, const Rectangle& value, std::vector<Rectangle>& result, int &diskAccesses) {
-    if(currentNode == nullptr)
-        return;
-    if(currentNode->isLeaf) {
-        bool currentIntersects = (*currentNode).intersects(currentNode->MBR, value);
-        if (currentIntersects) {
-            result.push_back(currentNode->MBR);
+    int j = pow(2,n); // Número de rectángulos
+    for (int i=0; i<j; i++){
+        int x1 = distr1(gen);
+        int y1 = distr1(gen);
+        int x2 = x1 + distr2(gen);
+        int y2 = y1 + distr2(gen);
+        if (x2>500000){
+            x2 = 500000;
         }
-    } else {
-        for (Node* child : currentNode->children) {
-            diskAccesses++;
-            if((*child).intersects(child->MBR, value)) {
-                searchRecursive(child, value, result, diskAccesses);
-            }
+        if (y2>500000){
+            y2 = 500000;
         }
+        Rectangle rect;
+        rect.x1 = x1;
+        rect.y1 = y1;
+        rect.x2 = x2;
+        rect.y2 = y2;
+
+        rects.push_back(rect);
+
+        //Imprime los rectángulos generados
+        //cout << "Rectangulo " << i + 1 << ": " << x1 << ", " << x2 << " " << y1 << ", " << y2 << endl;
     }
+    return rects;
+}    
+
+int main(){
+    // Se crea un vector de rectángulos
+    vector<Rectangle> rects = createRandomRectangles(15);
+    // Se hace el R-tree
+    RTree rtree = nearestX(rects, 4096);
+    // Se guarda el R-tree en disco
+    saveRTreeToFile(rtree);
+    // Se lee el R-tree desde disco
+    int diskAccesses = 0;
+    RTree rtree2 = readRTreeFromFile(diskAccesses);
+    // Se imprime el R-tree
+    cout << "Nodo raíz: " << rtree2.root->MBR.x1 << ", " << rtree2.root->MBR.x2 << endl;
+    cout << "Número de accesos a disco: " << diskAccesses << endl;
+
+
+    
+    return 0;
 }
-
-// 3. Implementar función de pruebas
-
-// cpp
-
-#include <chrono>
-#include <cmath>
-#include <ostream>
-using namespace std;
-
-void performTests() {
-    for(int i = 10; i <= 25; i++) {
-        int size = pow(2, i);
-        vector<Rectangle> rects;
-        // Aquí debes agregar el código para generar los Rectángulos
-
-        // Creamos el R-tree
-        RTree rtree = nearestX(rects, size);
-
-        // Guardamos el R-tree en disco
-
-        void performTests1() {
-            for(int i = 10; i <= 25; i++) {
-                int size = pow(2, i);
-                vector<Rectangle> rects;
-                // Aquí debes agregar el código para generar los Rectángulos
-
-                // Creamos el R-tree
-                RTree rtree = nearestX(rects, size);
-
-                // Guardamos el R-tree en disco
-                saveRTreeToFile(rtree);
-
-                // Realizamos las consultas
-                int totalDiskAccesses = 0;
-                auto start = std::chrono::high_resolution_clock::now(); // declare start variable here
-                for(int j = 0; j < 100; j++) {
-                    int diskAccesses = 0;
-                    Rectangle query; // Define tu consulta aquí
-                    RTree diskTree = readRTreeFromFile(diskAccesses);
-                    vector<Rectangle> results;
-                    diskTree.search(query, results, diskAccesses);
-                    totalDiskAccesses += diskAccesses;
-                }
-                auto finish = std::chrono::high_resolution_clock::now();
-
-                auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(finish - start);
-                cout << "Size: " << size << ", Time: " << elapsed.count() << ", Disk Accesses (avg): " << totalDiskAccesses / 100.0 << endl;
-            }
-        }
-    }
-}
-
-// Este código te brinda una idea de cómo puedes estructurar tu solución, 
-// pero es probable que necesites hacer ajustes para que funcione exactamente como lo necesitas.
-// También es importante tener en cuenta la eficiencia y la gestión de memoria cuando trabajas con estructuras
-// de datos grandes como los R-trees. ¡Buena suerte!
