02368951074b3b63074b3dc0e749f36e1da3c351
JavieraJimenezR <javiera.jimenez.rojas.99@gmail.com>
2023-10-19 00:57:52 -0300

    funciona leer por bloques segmentados

--- ArchivoLeer.cpp
+++ ArchivoLeer.cpp
@@ -1,36 +1,40 @@
 #include "Structures.hpp"
 using namespace std;
+#include <fstream>
 
 int main(){
     //leer todos los arboles de un factor
-    int factor = 10;
+    int factor = 12;
     int nivel = ceil((log10(pow(2,  factor)) / log10(1024))) ;
+    ofstream results("resultados.txt");
+    if(results.is_open()){
+        for(int i = 0; i<=nivel; i++){
+            //Abrir el archivo que contiene al nodo
+            string filename = "binNX/groupsNX" +to_string(factor)+ "Nivel" + to_string(i+1) + ".bin";
+            cout << filename << endl;
+            results << filename << endl;
+            FILE* arch = fopen(filename.c_str(), "rb");
+            if (arch == NULL){
+                cout << "Error al abrir el archivo" << endl;
+                return 1;
+            }
+            int nodosporleer=pow(2,i);
+            if (i>=1){
+                nodosporleer=pow(2,factor%10+10*(i-1));
+                if(factor%10==0){
+                    nodosporleer=pow(2,10*i);
+                }
+            }
+            results <<"nodos por leer corregido "<< nodosporleer << endl;
+            cout <<"nodos por leer corregido "<< nodosporleer << endl;
+            for (int i=0; i<nodosporleer; i++){
+                Node *node = new Node;
+                fread(node, sizeof(Node), 1, arch);        
+            }
+            fclose(arch);
 
-    for(int i = 0; i<=nivel; i++){
-        //Abrir el archivo que contiene al nodo
-        string filename = "binNX/groupsNX" +to_string(factor)+ "Nivel" + to_string(i+1) + ".bin";
-        cout << filename << endl;
-        FILE* arch = fopen(filename.c_str(), "rb");
-        if (arch == NULL){
-            cout << "Error al abrir el archivo" << endl;
-            return 1;
         }
-        int nodosporleer=(pow(2,10*i));
-        cout << nodosporleer << endl;
-        if (nodosporleer>pow(2,factor)){
-            nodosporleer=pow(2,factor);
-        }
-        cout <<"nodos por leer corregido "<< nodosporleer << endl;
-        for (int i=0; i<nodosporleer; i++){
-            Node *node = new Node;
-            fread(node, sizeof(Node), 1, arch);
-            cout <<i+1 <<" " <<node->MBR.centerX() << " " << node->MBR.centerY() << endl;
-            
-        }
-        fclose(arch);
-
     }
-
     return 0;
 
 }
